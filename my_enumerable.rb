ad325040ae9b2902c478dd9ebe2a9d9179264cb2
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-11 14:37:15 +0100

    fix linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,28 +1,29 @@
 module MyEnumerable
-    def all?
-      value = true
-      each do
-        @list
-          .each { |n| return false unless yield n }
-      end
-      value
+  def all?
+    value = true
+    each do
+      @list
+        .each { |n| return false unless yield n }
     end
-  
-    def any?
-      value = false
-      each do
-        @list
-          .each { |n| return true if yield n }
-      end
-      value
-    end
-  
-    def filter
-      value = []
-      each do
-        @list
-          .each { |n| value.push(n) if yield n }
-      end
-      value
-    end
+    value
   end
+
+def any?
+  value = false
+  each do
+    @list
+      .each { |n| return true if yield n }
+  end
+  value
+end
+
+  def filter
+    value = []
+    each do
+      @list
+        .each { |n| value.push(n) if yield n }
+    end
+    value
+  end
+  
+end
